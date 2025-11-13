eea979dca8d1b19ef4f558a5d5febf0aa734aa13
Tommy Han <tommyhonym@gmail.com>
2025-11-13 18:35:14 +0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FluidTabAccessory",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "FluidTabAccessory",
+            targets: ["FluidTabAccessory"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "FluidTabAccessory"
+        ),
+        .testTarget(
+            name: "FluidTabAccessoryTests",
+            dependencies: ["FluidTabAccessory"]
+        ),
+    ]
+)
